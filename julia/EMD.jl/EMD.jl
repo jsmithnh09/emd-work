b6ef028a65533e6ceda0f182ce79bdb3be70ed60
Jordan S <46058723+jsmithnh09@users.noreply.github.com>
2022-04-09 13:20:38 -0400

    Adding `jl` specific files.

---
+++ julia/EMD.jl/EMD.jl
@@ -0,0 +1,14 @@
+"""
+Empirical Mode Decomposition Package.
+"""
+module EMD
+
+import DSP, Distributions, Interpolations
+using Statistics: mean
+
+## include environment setup.
+include("config.jl")
+
+EMD
+
+end # module
