e3824b89dd84781e434829fb4c516e39a876d96c
Jordan S <46058723+jsmithnh09@users.noreply.github.com>
2022-05-18 21:20:03 -0400

    Adding Stats and kwargs.

--- julia/EMD.jl/src/config.jl
+++ julia/EMD.jl/src/config.jl
@@ -13,33 +13,34 @@ sifting, maximum iterations, the type of interpolation between extrema, etc.
 """
 
 struct EMDConfig
+    input::Vector
     maxiters::Int
     maxmodes::Int
     ndirs::Int
-    stop::Vector{AbstractFloat}
+    stop::Vector
     interp::InterpMode
     function EMDConfig(
-        maxiters=2_000, 
-        maxmodes=typemax(Int), 
-        ndirs=4, 
-        stop=[0.05, 0.5, 0.05], 
-        interp=Cubic
+        x::Vector,
+        maxiters::Int=2_000, 
+        maxmodes::Int=typemax(Int), 
+        ndirs::Int=4, 
+        stop::Vector=[0.05, 0.5, 0.05], 
+        interp::InterpMode=Cubic
         )
         if length(stop) != 3
             throw(DimensionMismatch("stop vector must be 3-elements, instead got $(size(stop))"))
         end
-        return new(maxiters, maxmodes, ndirs, stop, interp)
+        return new(x, maxiters, maxmodes, ndirs, stop, interp)
     end
 end
 
 ### Show details about the config struct.
 function Base.show(io::IO, cfg::EMDConfig)
     mmstr = (cfg.maxmodes == typemax(Int)) ? "∞" : "$(cfg.maxmodes)"
-    print(io, "*** EMD Config ***\n") 
-    print(io, "------------------\n")
-    print(io, "maxmodes: ", mmstr, "\n")
-    print(io, "stop: ", cfg.stop, "\n") 
-    print(io, "interp: ", string(cfg.interp), "\n")
+    print(io, "EMD Config:\n") 
+    print(io, "maxmodes = ", mmstr, "\n")
+    print(io, "stop = ", cfg.stop, "\n") 
+    print(io, "interp = ", string(cfg.interp), "\n")
 end
 
 end # module
